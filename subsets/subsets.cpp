28036ac1c8cb31fda0706f4f7b8ccd54d017fb66
Game Gamemod <kopristupin1@gmail.com>
2020-09-22 14:58:37 +0300

    Добавьте файлы проекта.

---
+++ subsets/subsets.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+#include<vector>
+using namespace std;
+vector<char> subset;
+void search(vector<char> v, int n, int k) {
+    if (k == v.size()) {
+        if (subset.size() == n) {
+        for (auto it : subset) {
+
+            cout << it;
+        }
+        cout << "\n";
+    }
+    }
+    else {
+        subset.push_back(v[k]);
+        search(v, n,k+1);
+        subset.pop_back();
+        search(v, n,k + 1);
+    }
+}
+
+int main()
+{
+    vector<char> v{'a', 'd', 'e', 'f'};
+    search(v, 2, 0);
+   
+}
+
